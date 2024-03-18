d97dcf2ab5d6dd5e378ce145b12165e408d1f694
MartinLudwig <martin.j.ludwig@gmx.de>
2024-03-18 15:10:43 +0100

    Auto-Fill Table on load

---
+++ src/main/resources/static/js/simple.js
@@ -0,0 +1,66 @@
+function fillTodoTable() {
+
+    console.log("fillTodoTable function-Start");
+
+    fetch("http://localhost:8080/todos")
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Data received:', data);
+            let table = document.querySelector('#todoTable');
+
+            let tableHead = document.createElement('thead');
+            let headRow = document.createElement('tr');
+            let headRow1 =document.createElement('td');
+            let headRow2 =document.createElement('td');
+            let headRow3 =document.createElement('td');
+            let headRow4 =document.createElement('td');
+            let headRow5 =document.createElement('td');
+            headRow1.textContent = 'Description';
+            headRow2.textContent = 'Create Date';
+            headRow3.textContent = 'Done Date';
+            headRow4.textContent = 'Is Done';
+            headRow5.textContent = 'Actions';
+            headRow.appendChild(headRow1);
+            headRow.appendChild(headRow2);
+            headRow.appendChild(headRow3);
+            headRow.appendChild(headRow4);
+            headRow.appendChild(headRow5);
+            tableHead.appendChild(headRow);
+            table.appendChild(tableHead);
+
+
+            let todos = data.content;
+            console.log('Content:', data.content);
+            for (let i = 0; i <todos.length; i++) {
+                let tr = document.createElement('tr');
+
+                let td1 = document.createElement('td');
+                td1.textContent = todos[i].description;
+                tr.appendChild(td1);
+                let td2 = document.createElement('td');
+                td2.textContent = todos[i].createDate;
+                tr.appendChild(td2);
+                let td3 = document.createElement('td');
+                td3.textContent = todos[i].doneDate;
+                tr.appendChild(td3);
+                let td4 = document.createElement('td');
+                td4.textContent = todos[i].done;
+                tr.appendChild(td4);
+                let td5 = document.createElement('td');
+                td5.textContent = "actions-Dummy";
+                tr.appendChild(td5);
+
+                table.appendChild(tr);
+            }
+
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+
+}
